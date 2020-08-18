2ff2850bd6a0ffb1c865e82034297161a6a24594
Indra <indra@imceria.com>
2020-08-18 17:38:56 +0700

    Mapping Modules

---
+++ app/Http/Controllers/Produk/Produk.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Controllers\Produk;
+
+use App\ClassesRule\CRUDInterface;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class Produk extends Controller implements CRUDInterface
+{
+    //
+    public function index()
+    {
+        // TODO: Implement index() method.
+    }
+
+    public function add()
+    {
+        // TODO: Implement add() method.
+    }
+
+    public function update($id)
+    {
+        // TODO: Implement update() method.
+    }
+
+    public function update_process(Request $req, $id)
+    {
+        // TODO: Implement update_process() method.
+    }
+
+    public function add_process(Request $req)
+    {
+        // TODO: Implement add_process() method.
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,7 +16,7 @@ use Illuminate\Support\Facades\Route;
 Route::get("/","Auth@index")->name("login");
 Route::post("/login","Auth@login")->name("login.post");
 
-//Modul Akun
+//Modul Akun > akun.
 Route::middleware("gateway:0|1|2")->prefix("akun")->name("akun.")->group(function (){
     Route::get("/","Akun@index")->name("list");
     Route::get("/add","Akun@akun_add")->name("add");
@@ -24,13 +24,53 @@ Route::middleware("gateway:0|1|2")->prefix("akun")->name("akun.")->group(functio
     Route::get("/update/{id}","Akun@akun_update")->name("update");
     Route::post("/update/{id}","Akun@akun_update_process")->name("update.process");
 });
-
+//Modul Pelanggan > pelanggan.
 Route::middleware("gateway:1")->prefix("pelanggan")->name("pelanggan.")->group(function (){
     Route::get("/","Pelanggan@index")->name("list");
     Route::get("/add","Pelanggan@add")->name("add");
     Route::post("/add","Pelanggan@add_process")->name("add.process");
     Route::get("/update/{id}","Pelanggan@update")->name("update");
     Route::post("/update/{id}","Pelanggan@update_process")->name("update.process");
+});
+//Modul Produk > produk.
+Route::middleware("gateway:1")->prefix("produk")->namespace("Produk")->name("produk.")->group(function (){
+    Route::get("/","Produk@index")->name("list");
+    Route::get("/add","Produk@add")->name("add");
+    Route::post("/add","Produk@add_process")->name("add.process");
+    Route::get("/update/{id}","Produk@update")->name("update");
+    Route::post("/update/{id}","Produk@update_process")->name("update.process");
+    //Sub Module Size >  produk.util
+    Route::prefix("size")->name("size.")->group(function (){
+        Route::get("/","Size@index")->name("list");
+        Route::get("/add","Size@add")->name("add");
+        Route::post("/add","Size@add_process")->name("add.process");
+        Route::get("/update/{id}","Size@update")->name("update");
+        Route::post("/update/{id}","Size@update_process")->name("update.process");
+    });
+});
+//Modul POS > pos.
+Route::middleware("gateway:1")->prefix("pos")->namespace("Pos")->name("pos.")->group(function (){
+    Route::get("/","Main@index")->name("menus");
+    //Sub Module Size >  produk.util
+    Route::prefix("api")->name("api.")->group(function (){
+
+    });
+});
+//Modul Laporan
+Route::middleware("gateway:0|1|2")->prefix("laporan")->namespace("Laporan")->name("laporan.")->group(function (){
+    Route::get("/","Main@index")->name("menus");
+    //Sub Module Laporan >  laporan.keuangan
+    Route::prefix("keuangan")->middleware("gateway:2")->name("keuangan.")->group(function (){
+
+    });
+    //Sub Module Laporan >  laporan.penjualan
+    Route::prefix("penjualan")->middleware("gateway:1")->name("penjualan.")->group(function (){
+
+    });
+    //Sub Module Laporan >  laporan.pelanggan
+    Route::prefix("pelanggan")->middleware("gateway:1")->name("pelanggan.")->group(function (){
+
+    });
 });
 
 
