96d86b4c53ba5bb37046d37fc87ed2850aea32cb
Indra Gunanda <indra@imceria.com>
2020-08-18 23:23:01 +0700

    Modul Produk

--- app/Http/Controllers/Produk/Produk.php
+++ app/Http/Controllers/Produk/Produk.php
@@ -2,8 +2,11 @@
 
 namespace App\Http\Controllers\Produk;
 
+use App\Casts\StatusProduct;
 use App\ClassesRule\CRUDInterface;
 use App\Http\Controllers\Controller;
+use App\Models\Product;
+use App\Models\ProductSize;
 use Illuminate\Http\Request;
 
 class Produk extends Controller implements CRUDInterface
@@ -11,26 +14,86 @@ class Produk extends Controller implements CRUDInterface
     //
     public function index()
     {
-        // TODO: Implement index() method.
+        $data = Product::orderBy("created_at","desc")->get();
+        foreach ($data as $index => &$datum) {
+            $datum->status_lang = StatusProduct::lang($datum->status);
+        }
+        return view("produk.home",[
+            "title"=>"Data Produk",
+            "data"=>$data
+        ]);
     }
 
     public function add()
     {
-        // TODO: Implement add() method.
+        $sizes = ProductSize::all();
+        if (count($sizes) === 0){
+            return redirect(route("produk.size.add"))->withErrors(["msg"=>"Silahkan Isi Ukuran Terlebih Dahulu"]);
+        }
+        return view("produk.form",[
+            "title"=>"Tambah Produk",
+            "data"=>[],
+            "sizes"=>$sizes,
+            "disabled"=>[],
+            "route"=>route("produk.add.process")
+        ]);
     }
 
     public function update($id)
     {
-        // TODO: Implement update() method.
+        $find = Product::where(["id"=>$id]);
+        if ($find->count() == 0){
+            return back()->withErrors(["msg"=>"Data Tidak Ditemukan"]);
+        }
+        $sizes = ProductSize::all();
+        return view("produk.form",[
+            "title"=>"Update Produk",
+            "data"=>$find->first(),
+            "sizes"=>$sizes,
+            "disabled"=>[],
+            "route"=>route("produk.update.process",$id)
+        ]);
+
     }
 
     public function update_process(Request $req, $id)
     {
-        // TODO: Implement update_process() method.
+        $req->validate([
+            "name"=>"required",
+            "size_id"=>"required|exists:product_sizes,id",
+            "stock"=>"numeric",
+            "price"=>"numeric|required"
+        ]);
+        $data = $req->all();
+        unset($data["_token"]);
+        $data["status"] = StatusProduct::ACTIVE;
+        $data["user_id"] = session()->get("id");
+
+        $make = Product::where(["id"=>$id])->update($data);
+        if ($make){
+            return back()->with(["msg"=>"Produk Berhasil Di Tambahkan","url"=>route("produk.list")]);
+        }else{
+            return back()->with(["msg"=>"Produk Gagal Di Tambahkan"]);
+        }
     }
 
     public function add_process(Request $req)
     {
-        // TODO: Implement add_process() method.
+        $req->validate([
+            "name"=>"required",
+            "size_id"=>"required|exists:product_sizes,id",
+            "stock"=>"numeric",
+            "price"=>"numeric|required"
+        ]);
+        $data = $req->all();
+        $data["status"] = StatusProduct::ACTIVE;
+        $data["user_id"] = session()->get("id");
+
+        $make = Product::create($data);
+        if ($make){
+            return back()->with(["msg"=>"Produk Berhasil Di Tambahkan","url"=>route("produk.list")]);
+        }else{
+            return back()->with(["msg"=>"Produk Gagal Di Tambahkan"]);
+        }
     }
 }
--- app/Http/Controllers/Produk/Size.php
+++ app/Http/Controllers/Produk/Size.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Produk;
 
 use App\ClassesRule\CRUDInterface;
 use App\Http\Controllers\Controller;
+use App\Models\ProductSize;
 use Illuminate\Http\Request;
 
 class Size extends Controller implements CRUDInterface
@@ -11,26 +12,71 @@ class Size extends Controller implements CRUDInterface
     //
     public function index()
     {
-        // TODO: Implement index() method.
+        $data = ProductSize::orderBy("created_at","desc")->get();
+        return view("produk.size.home",[
+            "title"=>"Data Ukuran",
+            "data"=>$data
+        ]);
     }
 
     public function add()
     {
-        // TODO: Implement add() method.
+        return view("produk.size.form",[
+            "title"=>"Tambah Ukuran",
+            "data"=>[],
+            "disabled"=>[],
+            "route"=>route("produk.size.add.process")
+        ]);
     }
 
     public function update($id)
     {
-        // TODO: Implement update() method.
+       $find = ProductSize::where(["id"=>$id]);
+       if ($find->count() == 0){
+           return back()->withErrors(["msg"=>"Data Tidak Ditemukan"]);
+       }
+
+        return view("produk.size.form",[
+            "title"=>"Update Size",
+            "data"=>$find->first(),
+            "disabled"=>[],
+            "route"=>route("produk.size.update.process",$id)
+        ]);
+
     }
 
     public function update_process(Request $req, $id)
     {
-        // TODO: Implement update_process() method.
+        $req->validate([
+            "name"=>"required"
+        ]);
+        $data = $req->all();
+        unset($data["_token"]);
+        $make = ProductSize::where(["id"=>$id])->update($data);
+        if ($make){
+            return  back()->with([
+                "msg"=>"Update Ukuran Sukses",
+                "url"=>route("produk.size.list")
+            ]);
+        }else{
+            return back()->withErrors(["msg"=>"Update Ukuran Sukses"]);
+        }
     }
 
     public function add_process(Request $req)
     {
-        // TODO: Implement add_process() method.
+        $req->validate([
+            "name"=>"required|unique:product_sizes,name"
+        ]);
+        $data = $req->all();
+        $make = ProductSize::create($data);
+        if ($make){
+            return  back()->with([
+                "msg"=>"Pembuatan Ukuran Sukses",
+                "url"=>route("produk.size.list")
+            ]);
+        }else{
+            return back()->withErrors(["msg"=>"Pembuatan Ukuran Sukses"]);
+        }
     }
 }
